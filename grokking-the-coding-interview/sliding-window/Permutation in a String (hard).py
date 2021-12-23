7626a391139dc16b4c90a98399ac58fd854fe8ca
guardianwang <962247283@qq.com>
2021-12-23 01:30:52 -0500

    lc number

--- grokking-the-coding-interview/sliding-window/Permutation in a String (hard).py
+++ grokking-the-coding-interview/sliding-window/Permutation in a String (hard).py
@@ -1,4 +1,5 @@
 """
+LC 567
 Given a string and a pattern, find out if the string contains any permutation of the pattern.
 
 Permutation is defined as the re-arranging of the characters of the string. For example, “abc” has the following six permutations:
