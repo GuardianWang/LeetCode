33661004497d5316ea8d7ef890353d99ebfdc5ad
guardianwang <962247283@qq.com>
2021-12-22 22:46:20 -0500

    init

---
+++ grokking-the-coding-interview/sliding-window/Words Concatenation (hard).py
@@ -0,0 +1,29 @@
+"""
+Given a string and a list of words, find all the starting indices of substrings in the given string that are a concatenation of all the given words exactly once without any overlapping of words. It is given that all words are of the same length.
+
+Example 1:
+
+Input: String="catfoxcat", Words=["cat", "fox"]
+Output: [0, 3]
+Explanation: The two substring containing both the words are "catfox" & "foxcat".
+Example 2:
+
+Input: String="catcatfoxfox", Words=["cat", "fox"]
+Output: [3]
+Explanation: The only substring containing both the words is "catfox".
+"""
+
+
+def find_word_concatenation(str1, words):
+    sum_len = sum(map(len, words))
+    for win_end, c in enumerate(words):
+
+
+
+def main():
+  print(find_word_concatenation("catfoxcat", ["cat", "fox"]))
+  print(find_word_concatenation("catcatfoxfox", ["cat", "fox"]))
+
+
+main()
+
