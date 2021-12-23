61bcd91879527d863006e46decb040d10484398b
guardianwang <962247283@qq.com>
2021-12-23 01:27:54 -0500

    lc number

--- grokking-the-coding-interview/sliding-window/Longest Substring with Same Letters after Replacement (hard).py
+++ grokking-the-coding-interview/sliding-window/Longest Substring with Same Letters after Replacement (hard).py
@@ -1,4 +1,5 @@
 """
+LC 424
 Given a string with lowercase letters only, if you are allowed to replace no more than k letters with any letter, find the length of the longest substring having the same letters after replacement.
 
 Example 1:
