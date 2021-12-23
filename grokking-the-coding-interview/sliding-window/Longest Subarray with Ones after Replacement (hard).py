51874c64969c9749c405d41406233910cf666bb6
guardianwang <962247283@qq.com>
2021-12-23 01:29:13 -0500

    lc number

--- grokking-the-coding-interview/sliding-window/Longest Subarray with Ones after Replacement (hard).py
+++ grokking-the-coding-interview/sliding-window/Longest Subarray with Ones after Replacement (hard).py
@@ -1,4 +1,5 @@
 """
+LC 1004
 Given an array containing 0s and 1s, if you are allowed to replace no more than ‘k’ 0s with 1s, find the length of the longest contiguous subarray having all 1s.
 
 Example 1:
