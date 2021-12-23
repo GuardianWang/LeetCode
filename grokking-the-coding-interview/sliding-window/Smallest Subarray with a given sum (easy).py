e039bb1c7f3d0a57bd8e278b607d4417a06bd4e1
guardianwang <962247283@qq.com>
2021-12-23 02:00:41 -0500

    lc number

--- grokking-the-coding-interview/sliding-window/Smallest Subarray with a given sum (easy).py
+++ grokking-the-coding-interview/sliding-window/Smallest Subarray with a given sum (easy).py
@@ -1,4 +1,5 @@
 """
+LC 209
 Given an array of positive numbers and a positive number ‘S,’ find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0 if no such subarray exists.
 
 Example 1:
