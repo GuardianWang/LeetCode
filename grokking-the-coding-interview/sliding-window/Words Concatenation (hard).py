aa48c7ed4e194bf0344372598836badcb9e73894
guardianwang <962247283@qq.com>
2021-12-23 01:38:57 -0500

    lc number

--- grokking-the-coding-interview/sliding-window/Words Concatenation (hard).py
+++ grokking-the-coding-interview/sliding-window/Words Concatenation (hard).py
@@ -1,4 +1,5 @@
 """
+LC 30
 Given a string and a list of words, find all the starting indices of substrings in the given string that are a concatenation of all the given words exactly once without any overlapping of words. It is given that all words are of the same length.
 
 Example 1:
