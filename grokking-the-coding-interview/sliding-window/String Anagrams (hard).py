b5b36578b8b67db3e38a7c9a44aae7b766a7a8ce
guardianwang <962247283@qq.com>
2021-12-23 01:34:47 -0500

    lc number

--- grokking-the-coding-interview/sliding-window/String Anagrams (hard).py
+++ grokking-the-coding-interview/sliding-window/String Anagrams (hard).py
@@ -1,4 +1,5 @@
 """
+LC 438
 Given a string and a pattern, find all  of the pattern in the given string.
 
 Every anagram is a permutation of a string. As we know, when we are not allowed to repeat characters while finding permutations of a string, we get N!N! permutations (or anagrams) of a string having NN characters. For example, here are the six anagrams of the string “abc”:
