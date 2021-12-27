5543adf39db05253d8112fd31d76000563881862
guardianwang <962247283@qq.com>
2021-12-27 17:31:02 -0500

    answer

---
+++ grokking-the-coding-interview/merge-intervals/Merge Intervals (medium).py
@@ -0,0 +1,75 @@
+"""
+LC 56
+Given a list of intervals, merge all the overlapping intervals to produce a list that has only mutually exclusive intervals.
+
+Example 1:
+
+Intervals: [[1,4], [2,5], [7,9]]
+Output: [[1,5], [7,9]]
+Explanation: Since the first two intervals [1,4] and [2,5] overlap, we merged them into 
+one [1,5].
+Example 2:
+
+Intervals: [[6,7], [2,4], [5,9]]
+Output: [[2,4], [5,9]]
+Explanation: Since the intervals [6,7] and [5,9] overlap, we merged them into one [5,9].
+
+Example 3:
+
+Intervals: [[1,4], [2,6], [3,5]]
+Output: [[1,6]]
+Explanation: Since all the given intervals overlap, we merged them into one.
+"""
+
+
+class Interval:
+  def __init__(self, start, end):
+    self.start = start
+    self.end = end
+
+  def print_interval(self):
+    print("[" + str(self.start) + ", " + str(self.end) + "]", end='')
+
+  def __str__(self):
+    return "[" + str(self.start) + ", " + str(self.end) + "]"
+
+
+def merge(intervals):
+    # sort
+    intervals = sorted(intervals, key=lambda x: x.start)
+    left, overlap = 0, 0
+    res = [intervals[left]]
+    for overlap in range(1, len(intervals)):
+        if intervals[overlap].start <= intervals[left].end:
+            intervals[left].end = max(intervals[overlap].end, intervals[left].end)
+        else:
+            left = overlap
+            res.append(intervals[left])
+    return res
+
+
+def main():
+  print("Merged intervals: ", end='')
+  for i in merge([Interval(1, 4), Interval(2, 5), Interval(7, 9)]):
+    i.print_interval()
+  print()
+
+  print("Merged intervals: ", end='')
+  for i in merge([Interval(6, 7), Interval(2, 4), Interval(5, 9)]):
+    i.print_interval()
+  print()
+
+  print("Merged intervals: ", end='')
+  for i in merge([Interval(1, 4), Interval(2, 6), Interval(3, 5)]):
+    i.print_interval()
+  print()
+
+
+main()
+
+
+"""
+Time O(NlogN)
+Space O(N)
+"""
+
