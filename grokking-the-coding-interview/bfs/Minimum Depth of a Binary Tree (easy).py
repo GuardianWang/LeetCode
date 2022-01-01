91e0d7d199ab2d0d1f34f716410820a55f969eb1
guardianwang <962247283@qq.com>
2022-01-01 05:03:35 -0500

    lc num

--- grokking-the-coding-interview/bfs/Minimum Depth of a Binary Tree (easy).py
+++ grokking-the-coding-interview/bfs/Minimum Depth of a Binary Tree (easy).py
@@ -1,4 +1,5 @@
 """
+LC 111
 Find the minimum depth of a binary tree. The minimum depth is the number of nodes along the shortest path from the root node to the nearest leaf node.
 """
 from collections import deque
