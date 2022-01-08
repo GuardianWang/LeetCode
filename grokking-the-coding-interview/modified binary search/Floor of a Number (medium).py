c217b7c2af600136949528f003b5615b849992ee
guardianwang <962247283@qq.com>
2022-01-08 18:20:22 -0500

    answer

---
+++ grokking-the-coding-interview/modified binary search/Floor of a Number (medium).py
@@ -0,0 +1,55 @@
+"""
+Given an array of numbers sorted in ascending order, find the floor of a given number ‘key’. The floor of the ‘key’ will be the biggest element in the given array smaller than or equal to the ‘key’
+
+Write a function to return the index of the floor of the ‘key’. If there isn’t a floor, return -1.
+
+Example 1:
+
+Input: [4, 6, 10], key = 6
+Output: 1
+Explanation: The biggest number smaller than or equal to '6' is '6' having index '1'.
+Example 2:
+
+Input: [1, 3, 8, 10, 15], key = 12
+Output: 3
+Explanation: The biggest number smaller than or equal to '12' is '10' having index '3'.
+Example 3:
+
+Input: [4, 6, 10], key = 17
+Output: 2
+Explanation: The biggest number smaller than or equal to '17' is '10' having index '2'.
+Example 4:
+
+Input: [4, 6, 10], key = -1
+Output: -1
+Explanation: There is no number smaller than or equal to '-1' in the given array.
+"""
+def search_floor_of_a_number(arr, key):
+    if key < arr[0]:
+        return -1
+    l, r = 0, len(arr) - 1
+    while l <= r:
+        m = (l + r) >> 1
+        if key < arr[m]:
+            r = m - 1
+        elif key > arr[m]:
+            l = m + 1
+        else:
+            return m
+    return r
+
+
+def main():
+  print(search_floor_of_a_number([4, 6, 10], 6))
+  print(search_floor_of_a_number([1, 3, 8, 10, 15], 12))
+  print(search_floor_of_a_number([4, 6, 10], 17))
+  print(search_floor_of_a_number([4, 6, 10], -1))
+
+
+main()
+
+
+"""
+Time O(logN)
+Space O(1)
+"""
--- grokking-the-coding-interview/modified binary search/README.md
+++ grokking-the-coding-interview/modified binary search/README.md
@@ -6,12 +6,12 @@
 [[LC704](https://leetcode.com/problems/binary-search/)]
 1. [Ceiling of a Number (medium)]()
 [[LC35](https://leetcode.com/problems/search-insert-position/)]
+1. [Floor of a Number (medium)]()
+[[link](https://www.geeksforgeeks.org/floor-in-a-sorted-array/)]
 1. [Next Letter (medium)]()
 [[LC744](https://leetcode.com/problems/find-smallest-letter-greater-than-target/)]
 1. [Number Range (medium)]()
 [[LC34](https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/)]
-1. []()
-[[]()]
 1. []()
 [[]()]
 1. []()
