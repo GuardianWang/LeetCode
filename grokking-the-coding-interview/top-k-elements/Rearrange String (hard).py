c50cad533730f31d8c361a760bb1b5b4d0dbdbbc
guardianwang <962247283@qq.com>
2022-01-10 19:37:35 -0500

    correct

--- grokking-the-coding-interview/top-k-elements/Rearrange String (hard).py
+++ grokking-the-coding-interview/top-k-elements/Rearrange String (hard).py
@@ -1,5 +1,4 @@
 """
-LC 767
 Given a string, find if its letters can be rearranged in such a way that no two same characters ome next to each other.
 Example 1:
 Input: "aappp"
@@ -14,29 +13,37 @@ Input: "aapa"
 Output: ""
 Explanation: In all arrangements of "aapa", atleast two 'a' will come together e.g., "apaa", "paaa".
 """
-from collections import Counter
+from heapq import *
 
 
 def rearrange_string(s):
   # count
-  freqs = Counter(s)
-  # arrange
-  res = [''] * len(s)
-  l = 0  # next start
-  for c, f in freqs.most_common(len(freqs)):
-    # fill in either odd or even pos
-    if l + 2 * (f - 1) >= len(s):  # out of range
-      return ""
-    r = l
-    for _ in range(f):
-      if res[l] and r > 0 and not res[r - 1]:
-        l = r - 1  # update next start
-      res[r] = c
-      r += 2
-    if res[l]:  # update next start
-      l = r - 2
-      while l < len(s) and res[l]:
-        l += 1
+  freqs = {}
+  for c in s:
+    if c in freqs:
+      freqs[c] += 1
+    else:
+      freqs[c] = 1
+  # arrange 
+  # max heap, first deal with letters that often appears
+  h = [[-f, c] for c, f in freqs.items()]
+  heapify(h)
+  res = []
+  prev = None
+  while h:
+      if len(h) == 1 and prev is None and h[0][0] < -1:
+          return ""
+
+      top = heappop(h)
+      res.append(top[1])
+      top[0] += 1
+
+      if prev:
+          heappush(h, prev)
+      prev = top
+      if 0 == prev[0]:
+          prev = None
+
 
   return "".join(res)
 
@@ -48,3 +55,10 @@ def main():
 
 
 main()
+
+
+"""
+Time O(NlogN)
+Space O(N)
+"""
+
