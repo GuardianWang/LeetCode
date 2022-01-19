027c0757e4bbae624b3d488544216af1f81250ae
GuardianWang <31812793+GuardianWang@users.noreply.github.com>
2022-01-19 17:59:52 -0500

    Update Find-the-Duplicate-Number-(easy).py

--- grokking-the-coding-interview/cyclic-sort/Find-the-Duplicate-Number-(easy).py
+++ grokking-the-coding-interview/cyclic-sort/Find-the-Duplicate-Number-(easy).py
@@ -1,4 +1,5 @@
 """
+LC 287
 We are given an unsorted array containing ‘n+1’ numbers taken from the range 1 to ‘n’. The array has only one duplicate but it can be repeated multiple times. Find that duplicate number without using any extra space. You are, however, allowed to modify the input array.
 
 Example 1:
