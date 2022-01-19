beec17e0bcca3792cb7dfbaf8c924f6215d8bf95
guardianwang <962247283@qq.com>
2022-01-18 20:05:06 -0500

    modify

--- CS-Notes/data-structure/array-and-matrix/Kth Smallest Element in a Sorted Matrix ((Medium)).py
+++ CS-Notes/data-structure/array-and-matrix/Kth Smallest Element in a Sorted Matrix ((Medium)).py
@@ -6,7 +6,7 @@ Note that it is the kth smallest element in the sorted order, not the kth distin
 
 You must find a solution with a memory complexity better than O(n2).
 
- 
+
 
 Example 1:
 
@@ -18,25 +18,38 @@ Example 2:
 Input: matrix = [[-5]], k = 1
 Output: -5
 """
-from heapq import *
-
-
 class Solution:
     def kthSmallest(self, matrix, k: int) -> int:
-        h = [[matrix[i][0], i, 1] for i in range(min(k, len(matrix)))]  # min heap 
-        next_row = h[-1][1] + 1
-        for _ in range(k):
-            m, row, col = heappop(h)
-            if col < len(matrix[0]):
-                heappush(h, [matrix[row][col], row, col + 1])
-            if next_row < len(matrix):
-                heappush(h, [matrix[next_row][0], next_row, 0])
-                next_row += 1
-        return m
+        s, l = matrix[0][0], matrix[-1][-1]
+        while s < l:
+            m = (s + l) >> 1
+            cnt, s1, l1 = self.count_le(matrix, m)
+            if cnt < k:
+                s = l1
+            elif cnt > k:
+                l = s1
+            else:
+                return s1
+        return s
+
+    def count_le(self, matrix, m):
+        # this is O(N)
+        cnt = 0
+        r, c = len(matrix) - 1, 0
+        s, l = matrix[0][0], matrix[-1][-1]
+        while r >= 0 and c < len(matrix[0]):
+            if m < matrix[r][c]:
+                l = min(l, matrix[r][c])
+                r -= 1
+            else:
+                s = max(s, matrix[r][c])
+                c += 1
+                cnt += r + 1
+        return cnt, s, l  # largest number <= m and smallest > m
 
 
 """
-Time O(T+KlogT) T=min(N,K)
-Space O(T)
+Time O(Nlog(M-m))
+Space O(1)
 """
 
