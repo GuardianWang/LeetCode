f69be1bf43ed6e21058c2ab060616cee92f3ecfc
guardianwang <962247283@qq.com>
2022-01-20 19:09:15 -0500

    log

--- CS-Notes/algorithm/dynamic-programming/Climbing Stairs (Easy).py
+++ CS-Notes/algorithm/dynamic-programming/Climbing Stairs (Easy).py
@@ -24,15 +24,29 @@ Explanation: There are three ways to climb to the top.
 """
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <= 2:
-            return n
-        a, b = 1, 2
-        for _ in range(n - 2):
-            a, b = b, a + b
-        return b
+        q = [[1, 1], [1, 0]]  # q^n[0][0] is answer
+        return self.qpow(q, n)[0][0]
+
+    def qpow(self, q, n):
+        # binary n is 1 1 0 0 1
+        # q^n = q^(0 0 0 0 1) * q^(0 1 0 0 0) * q^(1 0 0 0 0)
+        power = 1
+        remain = [[1, 0], [0, 1]]
+        while power <= n:
+            if n & power:
+                remain = self.qmul(remain, q)
+            q = self.qmul(q, q)
+            power <<=1
+        return remain
+
+    def qmul(self, a, b):
+        return [[a[0][0] * b[0][0] + a[0][1] * b[1][0],
+                 a[0][0] * b[0][1] + a[0][1] * b[1][1]],
+                [a[1][0] * b[0][0] + a[1][1] * b[1][0],
+                 a[1][0] * b[0][1] + a[1][1] * b[1][1]]]
 
 
 """
-Time O(N)
+Time O(logN)
 Space O(1)
 """
