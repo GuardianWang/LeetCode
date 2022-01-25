ee94cde8677887ba54ca0c978c67c2e72fdc1272
guardianwang <962247283@qq.com>
2022-01-24 23:54:51 -0500

    lc number

--- check-in/daily/Next-Permutation-(Medium).py
+++ check-in/daily/Next-Permutation-(Medium).py
@@ -1,4 +1,5 @@
 """
+LC 31
 A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
 
 For example, for arr = [1,2,3], the following are considered permutations of arr: [1,2,3], [1,3,2], [3,1,2], [2,3,1].
