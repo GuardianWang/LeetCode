44cf6c16e905868c8d9cee97040f12e40fe0afd2
guardianwang <962247283@qq.com>
2022-02-25 23:24:07 -0500

    description

--- interview-exp/Circular-Printer.py
+++ interview-exp/Circular-Printer.py
@@ -1,6 +1,15 @@
 """
 https://leetcode.com/playground/SRN4cLVP
 
+A company has invented a new type of printing technology-a circular printer that
+ooks like this:
+
+It is a circular printer wheel with the letters A through Z in sequence. It wraps so A
+and Z are adjacent. The printer has a pointer that is initially at 'A'. Moving from any
+character to any adjacent character takes 1 second. It can move in either direction.
+Given a string of letters, what is the minimum time needed to print the string?
+(Note: Assume that printing does not take any time. Only consider the time it takes
+for the pointer to move.)
 """
 from itertools import *
 
