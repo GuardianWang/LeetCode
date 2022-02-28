084544b4c5dfa7bf68da043a7525aceac35244f1
guardianwang <962247283@qq.com>
2022-02-27 19:10:06 -0500

    comment

--- interview-exp/Image-Smoother-(Easy).py
+++ interview-exp/Image-Smoother-(Easy).py
@@ -1,7 +1,7 @@
 """
 LC 661
 An image smoother is a filter of the size 3 x 3 that can be applied to each cell of an image by rounding down the average of the cell and the eight surrounding cells (i.e., the average of the nine cells in the blue smoother). If one or more of the surrounding cells of a cell is not present, we do not consider it in the average (i.e., the average of the four cells in the red smoother).
-
+Can use highest bits to store value
 
 Given an m x n integer matrix img representing the grayscale of an image, return the image after applying the smoother on each cell of it.
 
